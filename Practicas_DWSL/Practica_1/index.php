5b31eafe7e8891629f8eed8b0eb9e8f550b26def
Josue-Montoya <142867456+Josue-Montoya@users.noreply.github.com>
2024-09-11 21:44:04 -0600

    update practicas

---
+++ Practicas_DWSL/Practica_1/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    echo("hola");
+
+    $var1 = 1;
+    $var2 = "Aaron";
+    $var3 = 'Fagoaga';
+    $var4 = 1.3456;
+    $var5 = true;
+
+    var_dump($var1);
+    var_dump($var2);
+    var_dump($var3);
+    var_dump($var4);
+    var_dump($var5);
+
+    echo $var2 . "<br>";
+    print_r($var3);
+
+    echo $var2 . $var3 . $var4;
+
+    echo "<br>hola $var2";
+
+    echo '<b style="color: red">Hola mundo<p>';
+
+?>
